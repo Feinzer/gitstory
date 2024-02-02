2d0bc1de916953aaa2ce9b68ff0ef79f92facff6
Daniel Hernández <danhernan23i@gmail.com>
2024-02-02 12:04:07 -0400

    feat: initial github api implementation
    * get branches

---
+++ lib/src/internal/github/api.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+import 'package:gitstory/env.local.dart';
+import 'package:gitstory/src/internal/models/branch.dart';
+import 'package:http/http.dart' as http;
+
+final Map<String, String> _headers = {
+  "Accept": "application/vnd.github+json",
+  "Authorization": "Bearer $accessToken",
+  "X-GitHub-Api-Version": "2022-11-28"
+};
+
+class GithubApi {
+  static Future<List<GithubBranch>> branches() async {
+    final url = Uri.parse("https://api.github.com/repos/$owner/$repo/branches");
+    final response = await http.get(
+      url,
+      headers: _headers,
+    );
+    if (response.statusCode == 200) {
+      final body = jsonDecode(response.body);
+      return List<GithubBranch>.generate(
+        body.length,
+        (index) => GithubBranch.fromMap(body[index]),
+      );
+    } else {
+      return [];
+    }
+  }
+}
